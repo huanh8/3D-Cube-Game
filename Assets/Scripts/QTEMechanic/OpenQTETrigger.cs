c1dc1e9d1745ce70786b704f08dcad2a2a9317f7
Zhi Pan <panz5@student.op.ac.nz>
2023-05-16 15:25:07 +1200

    Set up QTE trigger

--- Assets/Scripts/QTEMechanic/OpenQTETrigger.cs
+++ Assets/Scripts/QTEMechanic/OpenQTETrigger.cs
@@ -2,22 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+///Summary
+// set the tag of the object to player
+// this script is used to trigger the QTE event
+///Summary
 public class OpenQTETrigger : MonoBehaviour
 {
     public QTEManager QTEManager;
     public GameObject notice;
+    bool _isTriggered = false;
     // TODO: Notice animation for task (QTE event)
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
+        OnEnableQTE();
+    }
+
+    private void OnEnableQTE()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && _isTriggered)
+        {
+            Debug.Log("F pressed");
+            QTEManager.StartQTE();
+            notice.SetActive(false);
+            _isTriggered = false;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered"+ other.tag);
         if (other.CompareTag("Player"))
         {
             notice.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                QTEManager.StartQTE();
-                notice.SetActive(false);
-            }
+            _isTriggered = true;
         }
     }
+
 }
