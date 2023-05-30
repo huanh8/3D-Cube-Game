f09915daba58f26d154e01f1b85fa8d1807874c0
Zhi Pan <panz5@student.op.ac.nz>
2023-05-30 14:57:41 +1200

    Let QTE smoother

--- Assets/Scripts/QTEMechanic/QTEManager.cs
+++ Assets/Scripts/QTEMechanic/QTEManager.cs
@@ -7,16 +7,25 @@ public class QTEManager : MonoBehaviour
     public GameObject QTEBar;
     public GameObject ProgressBar;
 
-    public void StartQTE()
+    private GameObject _qteArea;
+
+    public void StartQTE(GameObject qteBox)
     {
         QTEBar.SetActive(true);
         ProgressBar.SetActive(true);
+        _qteArea = qteBox;
     }
 
     public void EndQTE()
     {
         QTEBar.SetActive(false);
         ProgressBar.SetActive(false);
+        _qteArea.SetActive(false);
     }
 
+    public void QuitQTE()
+    {
+        QTEBar.SetActive(false);
+        ProgressBar.SetActive(false);
+    }
 }
